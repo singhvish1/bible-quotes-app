8def1b4be28be190c3e4d1b6392a450f841862e5
singhvish1 <vishvajitdhull@gmail.com>
2026-01-30 14:19:19 -0600

    Organize quotes on landing page by book with section headers

--- BibleQuotesApp/Views/HomeView.swift
+++ BibleQuotesApp/Views/HomeView.swift
@@ -4,20 +4,34 @@ struct HomeView: View {
 	let quotes = QuoteService.shared.quotes
 	@EnvironmentObject var favorites: FavoritesManager
 
+	var quotesByBook: [String: [Quote]] {
+		let grouped = Dictionary(grouping: quotes) { $0.book ?? "Unknown" }
+		return grouped.sorted { $0.key < $1.key }
+			.reduce(into: [String: [Quote]]()) { result, group in
+				result[group.key] = group.value.sorted { $0.id < $1.id }
+			}
+	}
+
 	var body: some View {
-		List(quotes) { quote in
-			NavigationLink(destination: QuoteDetailView(quote: quote)) {
-				VStack(alignment: .leading, spacing: 6) {
-					Text(quote.text)
-						.font(.body)
-						.lineLimit(2)
-					if let ref = quote.reference {
-						Text(ref)
-							.font(.caption)
-							.foregroundColor(.secondary)
+		List {
+			ForEach(quotesByBook.keys.sorted(), id: \.self) { book in
+				Section(header: Text(book).font(.headline).fontWeight(.bold)) {
+					ForEach(quotesByBook[book] ?? []) { quote in
+						NavigationLink(destination: QuoteDetailView(quote: quote)) {
+							VStack(alignment: .leading, spacing: 6) {
+								Text(quote.text)
+									.font(.body)
+									.lineLimit(2)
+								if let ref = quote.reference {
+									Text(ref)
+										.font(.caption)
+										.foregroundColor(.secondary)
+								}
+							}
+							.padding(.vertical, 6)
+						}
 					}
 				}
-				.padding(.vertical, 6)
 			}
 		}
 		.navigationTitle("All Quotes")
